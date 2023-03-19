d021a699bf4fc42571b96fa70d3e21557570460e
Momoe <mn2668@nyu.edu>
2023-03-19 17:10:02 +0800

    edit app file to run program on command line for testing database functionalities

--- src/main/java/com/nyu/App.java
+++ src/main/java/com/nyu/App.java
@@ -1,11 +1,72 @@
 package com.nyu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.CommandLineRunner;
+
+import com.nyu.repository.AccountRepository;
+import com.nyu.repository.ReportRepository;
+import com.nyu.repository.PostRepository;
+import com.nyu.repository.ReplyRepository;
+import com.nyu.model.Account;
 
 @SpringBootApplication
-public class App {
+@EnableMongoRepositories
+public class App implements CommandLineRunner{
+    @Autowired
+    AccountRepository accountRepo;
+    @Autowired
+    ReportRepository reportRepo;
+    @Autowired
+    PostRepository postRepo;
+    @Autowired
+    ReplyRepository replyRepo;
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+    }
+
+    public void run(String... args) {
+
+        // Clean up any previous data
+		accountRepo.deleteAll(); // Doesn't delete the collection
+        
+        System.out.println("-------------CREATE ACCONUT-------------------------------\n");
+        
+        createAccount();
+        
+        System.out.println("\n----------------SHOW ALL ACCOUNTS---------------------------\n");
+        
+        showAllAccounts();
+        
+        System.out.println("\n--------------DONE-----------------------------------\n");
+
+    }
+
+    // the following methods are for testing purposes
+    //CREATE
+    public void createAccount() {
+        System.out.println("Data creation started...");
+        accountRepo.save(new Account("nyu", "123", "xyz@gmail.com", "user"));
+        System.out.println("Data creation complete...");
+    }
+
+    public void showAllAccounts() {
+         
+        accountRepo.findAll().forEach(account -> System.out.println(getAccountDetails(account)));
+    }
+
+    public String getAccountDetails(Account account) {
+
+        System.out.println(
+        "Id: " + account.getId() + 
+        ", \nUsername: " + account.getUsername() + 
+        ", \nEmail: " + account.getEmail() +
+        ", \nRole: " + account.getRole()
+        );
+        
+        return "";
     }
 }
